6e09b3e5e9e279ef539fbcc2cd89eedc759a8170
Josh <35745910+Joshmh7128@users.noreply.github.com>
2023-08-31 16:28:18 -0400

    Randomized placement complete

--- Assets/Scripts/PlanetGenerator.cs
+++ Assets/Scripts/PlanetGenerator.cs
@@ -7,13 +7,15 @@ public class PlanetGenerator : MonoBehaviour
     // this script generates a whole planet for the game
 
     [SerializeField] float PlanetSize; // the size of our planet when generated
-
+    [SerializeField] float waveHeightContributor; // wave height contributor factor for our waves
+    [SerializeField] int waveDistributionPercentage; // what is the chance that we change our wave?
     // our tile types
     public enum TileTypes
     { rock, dirt, grass, ore, water }
 
     // the list of tile prefabs that we are going to use to instantiate our planet
     [SerializeField] List<GameObject> tilePrefabs = new List<GameObject>();
+    [SerializeField] List<int> tileWeights = new List<int>();
 
     // on start, generate our planet
     private void Start() => GeneratePlanet();
@@ -21,14 +23,72 @@ public class PlanetGenerator : MonoBehaviour
     // our generation function
     private void GeneratePlanet()
     {
+        // let's create some grid data
+        int[,] LookupGrid = new int[(int)PlanetSize,(int)PlanetSize];
+        // randomly move throughout the grid, and place weighted numbers in it to assign regions to the space
+        for (int lx = 0; lx < PlanetSize; lx++)
+            for (int ly = 0; ly < PlanetSize; ly++)
+            {
+                /*int li = Random.Range(0, 100);
+                if (li <= waveDistributionPercentage)*/
+                    LookupGrid[lx,ly] = ChooseNumber();
+            }
+
+
         // create a grid of blocks on the X and Z axes
         for (int x = 0; x < PlanetSize; x++)
             for (int z = 0; z < PlanetSize; z++)
             {
                 // get our y position as a combination of our x and z
+                float yPos = (Mathf.Sin(x) + Mathf.Cos(z)) * waveHeightContributor;
 
+                // place a tile according to the lookup grid's information
+                Instantiate(tilePrefabs[LookupGrid[x, z]], new Vector3(x, yPos, z), Quaternion.Euler(0, Random.Range(0, 5) * 90, 0));
 
-                Instantiate(tilePrefabs[Random.Range(0,tilePrefabs.Count)], new Vector3(x, 0, z), Quaternion.Euler(new Vector3(0,Random.Range((int)0,(int)5)*90f)), null);
+             }
+    }
+
+    int ChooseNumber()
+    {
+        // get the total size of our weights
+        int total = 0;
+        foreach (int t in tileWeights)
+            total += t;
+        // choose a random number from our total weight pool
+        int i = Random.Range(0, total);
+        Debug.Log(total);
+
+        // our total counting upwards
+        int tx = 0;
+        // then check against the pool
+        for (int x = 0; x < tileWeights.Count; x++)
+        {
+            // make sure we check the 0th entry
+            if (x == 0)
+            {
+                if (i < tx + tileWeights[x])
+                    return x;
             }
+
+            // check to see if it is between the current weight and the next weight
+            if (x + 1 < tileWeights.Count)
+            {
+                // if we are checking anything but the first or last index...
+                if (i > tx && i < tx + tileWeights[x] || i == tx || i == tileWeights[x])
+                    return x;
+            }
+            // make sure we are checking for something that is there...
+            if (x + 1 > tileWeights.Count)
+            {
+                if (i >= tx && i <= tx + tileWeights[x] + 1 || i == tx)
+                    return x;
+            }
+
+            // if we havent found the number, add our current weight to tx
+            tx += tileWeights[x];
+        }
+
+        // if we failed, return 5 for the red cube
+        return 5;
     }
 }
